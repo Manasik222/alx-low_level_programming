ae2cdcfbf3752d18c5f1370fd1219ff384a6fb59
here <here>
2022-10-13 12:43:52 -0700

    commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
 *main -Entry point
-*Return: Always 0 (success)
+*Return: Alwys 0 (success)
 */
-int main (void)
+int main(void)
 {
 	printf("size of a char: @zu byte(s)\n", sizeof(char));
 	printf("size of an int: @zu byte(s)\n", sizeof(int));
