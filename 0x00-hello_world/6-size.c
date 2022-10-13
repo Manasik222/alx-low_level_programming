66029cfa2d24b88f727b4ba68e361b61ca7a4db7
here <here>
2022-10-13 13:57:42 -0700

    commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@ int main(void)
 	long along;
 	long long alonglong;
 	float afloat;
-	printf("size of a char: %lu byte(s)\n", sizeof(acharacter));
-	printf("size of an int: %lu byte(s)\n", sizeof(ainteger));
-	printf("size of a long int: %lu byte(s)\n", sizeof(along));
+	printf("size of a char: %zu byte(s)\n", sizeof(acharacter));
+	printf("size of an int: %zu byte(s)\n", sizeof(ainteger));
+	printf("size of a long int: %zu byte(s)\n", sizeof(along));
 	printf("size of a long long int: %lu byte(s)\n", sizeof(alonglong));
-	printf("size of a float: %lu byte(s)\n", sizeof(afloat));
+	printf("size of a float: %zu byte(s)\n", sizeof(afloat));
 	return (0);
 }
