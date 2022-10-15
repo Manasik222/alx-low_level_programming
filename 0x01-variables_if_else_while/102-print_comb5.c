79b99c0a9317690ed0f832ea06cb00318270ab20
here <here>
2022-10-14 19:12:22 -0700

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,43 +1,35 @@
 #include <stdio.h>
 
 /**
- * main - Prints 3 combination of numbers
- * Return: Always (Success)
- */
+ *  * main-entry point
+ *   *
+ *    * Description: prints all possible combinations of two two-digit numbers
+ *     *
+ *      * Return:0 success, non zero fail
+ *       */
+
 int main(void)
 {
-	int c, i, k, j;
+		int i, j;
 
-	for (c = 48; c <= 57; c++)
-	{
-		for (i = 48; i <= 57; i++)
-		{
-			for (k = 48; k <= 57; k++)
-			{
-
-				for (j = 48; j <= 57; j++)
-				{
-					if (((k + j) > (c + i) &&  k >= c) || c < k)
+			for (i = 0; i < 99; i++)
 					{
-						putchar(c);
-						putchar(i);
-						putchar(' ');
-						putchar(k);
-						putchar(j);
-						if (c + i + k + j == 227 && c == 57)
-						{
-							break;
-						{
-							else
-						{
-							putchar(',');
-							putchar(' ');
-						}
-					}
-				}
-			}
-		}
-	}
-  	putchar('\n');
-	return (0);
+								for (j = i + 1; j <= 99; j++)
+											{
+															putchar('0' + i / 10);
+																		putchar('0' + i % 10);
+
+																					putchar(' ');
+
+																								putchar('0' + j / 10);
+																											putchar('0' + j % 10);
+
+																														if (i == 98 && j == 99)
+																																			break;
+																																	putchar(',');
+																																				putchar(' ');
+																																						}
+									}
+				putchar('\n');
+					return (0);
 }
