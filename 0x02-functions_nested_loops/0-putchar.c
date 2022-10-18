f7fe2dfeb82ac2f0fee8e9a16748cc5d67ad9863
here <here>
2022-10-18 11:27:44 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,19 @@
 #include "main.h"
-
 /**
- * main -print _putchar
- * is written by bwave
- * Return: 0
+ * main - printer putchar
+ * Return: 0 always
  */
-
 int main(void)
 {
-putchar('_');
-putchar('p');
-putchar('u');
-putchar('t');
-putchar('c');
-putchar('h');
-putchar('a');
-putchar('r');
-putchar('\n');
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchae('r');
+	_putchar('\n');
+
+	return (0);
 }
