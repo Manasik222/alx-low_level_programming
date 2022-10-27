810bd1b57caae3cb746983bdb94923dbc365d724
Manasik222 <soka7999@outlook.com>
2022-10-27 11:15:19 -0700

    task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+/*
+ * main - Entry point
+ */
 int main(void)
 {
 	int n;
@@ -8,13 +11,13 @@ int main(void)
 	a[2] = 1024;
 	p = &n;
 	/*
-	 * * write your line of code here...
-	 * * Remember:
-	 * * - you are not allowed to use a
-	 * * - you are not allowed to modify p
-	 * * - only one statement
-	 * * - you are not allowed to code anyth
-	 * * ing else than this line of code
+	 * write your line of code here...
+	 * Remember:
+	 * - you are not allowed to use a
+	 * - you are not allowed to modify p
+	 * - only one statement
+	 * - you are not allowed to code anyth
+	 * ing else than this line of code
 */
 	*(p + 5) = 98;
 	/* ...so that this prints 98\n */
